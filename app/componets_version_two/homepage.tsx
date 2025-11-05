d1cbdaf65aea5c16b777357f1191c8a5979ca458
Caleb Kao <calebkao2014@gmail.com>
2025-11-04 23:14:21 -0600

    home page

---
+++ app/componets_version_two/homepage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View>
+      <Text>Welcome to the Home Page!</Text>
+      <button>home</button>
+    </View>
+  );
+}
